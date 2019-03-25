1a05330bada56b4f2f724ed11841b90314c190d9
Viktor Nymark <vnymark@gmail.com>
2019-03-25 12:25:56 +0100

    Added functionality to read and sort the input file and create a new file with the result.

--- advent_of_code_4/Program.cs
+++ advent_of_code_4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace advent_of_code_4
 {
@@ -6,7 +8,9 @@ namespace advent_of_code_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var unsortedInput = @"C:\Users\Wezno\source\repos\advent_of_code_4\advent_of_code_4\input.txt";
+            var sortedInput = @"C:\Users\Wezno\source\repos\advent_of_code_4\advent_of_code_4\sorted_input.txt";
+            File.WriteAllLines(sortedInput, File.ReadLines(unsortedInput).OrderBy(s => s));
         }
     }
 }
